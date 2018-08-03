459cada188af4fe660b70d20d82ee829acbefdc5
Navin Kumar K <NaveenKumarK219@users.noreply.github.com>
2018-08-03 16:43:38 +0530

    extension updated

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -1,15 +1,34 @@
 chrome.extension.sendMessage({}, function(response) {
+	//var iteratorCount = 0;
 	var readyStateCheckInterval = setInterval(function() {
-	if (document.readyState === "complete") {
-		clearInterval(readyStateCheckInterval);
-
-		// ----------------------------------------------------------
-		// This part of the script triggers when page is done loading
-
-		$("iframe").remove();
-		console.log("Ads removed using Ad Remover!");
-		// ----------------------------------------------------------
-
-	}
-	}, 10);
+		//removeAds(readyStateCheckInterval, iteratorCount); //called every 1000ms 
+		if (document.readyState === "complete") {
+			clearInterval(readyStateCheckInterval);
+			// ----------------------------------------------------------
+			// This part of the script triggers when page is done loading
+			removeAds();
+			// ----------------------------------------------------------
+		}
+	}, 10000);
 });
+
+// code to remove ads.
+function removeAds(){
+	var adsIframes = $('iframe'); // gets all frame elements in the page.
+	var adsRemovedFlag = false;
+	for(i=0;i<adsIframes.length;i++){
+		console.log("iframe id recieved : "+adsIframes[i].id);
+		if(adsIframes[i].id.indexOf('google') > -1 || adsIframes[i].id.indexOf('ads') > -1){
+			console.log("iframe id replaced : "+adsIframes[i].id);
+			//Replace the Ad frame with following
+			$(adsIframes[i]).replaceWith("<h1 class=\'ad-logo\'>Ad Remover</h1><p class=\'ad-desc\'>Ads removed using Ad Remover &copy;Navin Kumar</p>");
+			adsRemovedFlag = true;
+		}
+	}
+	if(adsRemovedFlag == true){
+		console.log("Ads removed using Ad Remover!");
+	}
+	
+}
+
+
--- src/page_action/page_action.js
+++ src/page_action/page_action.js
@@ -1,9 +1,20 @@
 var removeAdButton = document.getElementById("remove-ad");
-
+// Code to remove Ads
+var jsCode = `  adsIframes = $('iframe'); 
+				for(i=0;i<adsIframes.length;i++){
+					console.log("iframe id recieved : "+adsIframes[i].id); 
+					if(adsIframes[i].id.indexOf('google') > -1 || adsIframes[i].id.indexOf('ads') > -1){
+						console.log("iframe id replaced : "+adsIframes[i].id); 
+						$(adsIframes[i]).replaceWith("<h1 class='ad-logo'>Ad Remover</h1><p class='ad-desc'>Ads removed using Ad Remover &copy;Navin Kumar</p>"); 
+					}
+				} 
+				console.log('Ads removed using Ad Remover!');
+`;
 removeAdButton.onclick = function(){
 	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
       chrome.tabs.executeScript(
           tabs[0].id,
-          {code: '$("iframe").remove();'});
+          {code: jsCode});
     });
 };
+
