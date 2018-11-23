6dfbeb4bdcbc45cc0b4507d645b43a1635e544c2
Navin Kumar K <NaveenKumarK219@users.noreply.github.com>
2018-11-23 17:36:32 +0530

    updated extension to version 2.0

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -1,16 +1,30 @@
 chrome.extension.sendMessage({}, function(response) {
-	//var iteratorCount = 0;
+	var iteratorCount = 0;
+	
+	// This will call the function at regular intervals
 	var readyStateCheckInterval = setInterval(function() {
-		//removeAds(readyStateCheckInterval, iteratorCount); //called every 1000ms 
+		// Ads are removed after page loading is finished
 		if (document.readyState === "complete") {
-			clearInterval(readyStateCheckInterval);
-			// ----------------------------------------------------------
-			// This part of the script triggers when page is done loading
+			iteratorCount++;	
 			removeAds();
-			// ----------------------------------------------------------
 		}
-	}, 10000);
+	}, 5000);
+	if(iteratorCount == 10){
+		clearInterval(readyStateCheckInterval);
+	}
 });
+// This will replace the Ad in the page
+/*ad_div = "<div class='ad-div'> \
+			<h1 class='ad-logo blinking'>Navin's Ad Remover</h1> \
+			<p class='ad-desc'>Ads removed using Ad Remover &copy;Navin Kumar</p> \
+		</div>";*/
+ad_div = "<div class='Ncontainer Ndiv-inline'>\
+		      <div class='Ntext1 Ndiv-inline'>NAVIN's</div> \
+					<div class='Ntext2 Ndiv-inline'> \
+					  <span>AD REMOVER</span>\
+					</div>\
+					<p class='Ntext3'>&copy;Navin Kumar</p>\
+		</div>";
 
 // code to remove ads.
 function removeAds(){
@@ -18,10 +32,12 @@ function removeAds(){
 	var adsRemovedFlag = false;
 	for(i=0;i<adsIframes.length;i++){
 		console.log("iframe id recieved : "+adsIframes[i].id);
-		if(adsIframes[i].id.indexOf('google') > -1 || adsIframes[i].id.indexOf('ads') > -1){
+		if(adsIframes[i].id.indexOf('google') > -1 || adsIframes[i].id.indexOf('ads') > -1 || adsIframes[i].id.indexOf('-ad') > -1
+			|| adsIframes[i].id.indexOf('ad-') > -1 || adsIframes[i].id.indexOf('_ad') > -1 || adsIframes[i].id.indexOf('ad_') > -1
+			|| adsIframes[i].id.indexOf('swift') > -1){
 			console.log("iframe id replaced : "+adsIframes[i].id);
 			//Replace the Ad frame with following
-			$(adsIframes[i]).replaceWith("<h1 class=\'ad-logo\'>Ad Remover</h1><p class=\'ad-desc\'>Ads removed using Ad Remover &copy;Navin Kumar</p>");
+			$(adsIframes[i]).replaceWith(ad_div);
 			adsRemovedFlag = true;
 		}
 	}
--- src/page_action/page_action.js
+++ src/page_action/page_action.js
@@ -3,7 +3,9 @@ var removeAdButton = document.getElementById("remove-ad");
 var jsCode = `  adsIframes = $('iframe'); 
 				for(i=0;i<adsIframes.length;i++){
 					console.log("iframe id recieved : "+adsIframes[i].id); 
-					if(adsIframes[i].id.indexOf('google') > -1 || adsIframes[i].id.indexOf('ads') > -1){
+					if(adsIframes[i].id.indexOf('google') > -1 || adsIframes[i].id.indexOf('ads') > -1 
+						|| adsIframes[i].id.indexOf('-ad') > -1 || adsIframes[i].id.indexOf('ad-') > -1 
+						|| adsIframes[i].id.indexOf('_ad') > -1 || adsIframes[i].id.indexOf('ad_') > -1){
 						console.log("iframe id replaced : "+adsIframes[i].id); 
 						$(adsIframes[i]).replaceWith("<h1 class='ad-logo'>Ad Remover</h1><p class='ad-desc'>Ads removed using Ad Remover &copy;Navin Kumar</p>"); 
 					}
